c262a18195e00c6013a574c952c04e12059604c6
Eyeball Edward <edwardyeung39@gmail.com>
2024-06-29 15:35:20 +1000

    process smartrak ID if available

--- test/bus-departures.test.mjs
+++ test/bus-departures.test.mjs
@@ -58,6 +58,7 @@ describe('The BusDepartures class', () => {
       let buses = new BusDepartures(stubAPI, 19810)
       
       await buses.fetch({ gtfs: true, maxResults: 1 })
+
       let departure737 = buses.find(dep => dep.runData.runRef === '29-737--1-MF15-14')
       expect(departure737.routeData.routeNumber).to.equal('737')
       expect(departure737.routeData.routeName).to.equal('Croydon Station - Monash University via Boronia & Knox City Shopping Centre & Glen Waverley')
@@ -71,6 +72,17 @@ describe('The BusDepartures class', () => {
       await buses.fetch({ gtfs: true, maxResults: 1 })
 
       expect(buses[0].runData.direction.directionName).to.equal('Dandenong')
+    })
+
+    it('Should parse the Smartrak ID if available (please come back 🥺)', async () => {
+      let stubAPI = new StubAPI('1', '2')
+      stubAPI.setResponses([ stubDepartureData ])
+      let buses = new BusDepartures(stubAPI, 19810)
+      
+      await buses.fetch({ gtfs: true, maxResults: 1 })
+
+      let departure862 = buses.find(dep => dep.runData.runRef === '41-862--1-MF1-34')
+      expect(departure862.runData.vehicle.smartrakID).to.equal(60202)
     })
 
   })
