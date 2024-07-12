7a4207a684cc7a34cbaadfa744768ac919655b80
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-12 16:26:09 +1000

    get metro pattern sample code

---
+++ sample/get-metro-pattern.mjs
@@ -0,0 +1,17 @@
+import { PTVAPIInterface } from '../lib/ptv-api-interface.mjs'
+import PTVAPI from '../lib/ptv-api.mjs'
+import config from './config.json' assert { type: 'json' }
+import inspect from './inspect.mjs'
+
+let ptvAPI = new PTVAPI(new PTVAPIInterface(config.devID, config.key))
+
+async function main() {
+  let tdn = process.argv[2]
+  if (!tdn) return console.error('Please provide a TDN!')
+
+  let trip = await ptvAPI.metro.getStoppingPatternFromTDN(tdn)
+
+  inspect(trip)
+}
+
+main()
