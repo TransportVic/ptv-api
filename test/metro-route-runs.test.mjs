c030ba79490d1f7751d02afd18c65cdb4e31600f
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-19 16:13:00 +1000

    backfill missing forming/formed by which occurs during disruptions

--- test/metro-route-runs.test.mjs
+++ test/metro-route-runs.test.mjs
@@ -36,6 +36,16 @@ describe('The getRouteRuns function of the MetroInterface', () => {
     expect(tdn1697.position.properties.bearing).to.equal(186.26073629390189)
     expect(tdn1697.vehicle.motorCars).to.have.members(['217M', '218M', '917M', '918M'])
     expect(tdn1697.vehicle.model).to.equal('Xtrapolis')
+
+    expect(tdn1697.formedBy).to.deep.equal({
+      tdn: '3202',
+      runRef: '951202'
+    })
+
+    expect(tdn1697.forming).to.deep.equal({
+      tdn: '1108',
+      runRef: '949108'
+    })
   })
 
   it('Should not include the details of runs from other routes added in through the forming data', async () => {
@@ -50,5 +60,25 @@ describe('The getRouteRuns function of the MetroInterface', () => {
 
     let tdn3202 = runs.find(run => run.tdn === '3202')
     expect(tdn3202).to.be.undefined
+  })
+
+  it('Should backfill forming/formed by data if it only goes one way', async () => {
+    let stubAPI = new StubAPI()
+    stubAPI.setResponses([ stubMDDRunData ])
+    let ptvAPI = new PTVAPI(stubAPI)
+
+    let runs = await ptvAPI.metro.getRouteRuns(5, {
+      expand: ['VehicleDescriptor', 'VehiclePosition'],
+      includeForming: true
+    })
+
+    let tdn1108 = runs.find(run => run.tdn === '1108')
+    expect(tdn1108).to.not.be.undefined
+    expect(tdn1108.formedBy).to.deep.equal({
+      tdn: '1697',
+      runRef: '949697'
+    })
+
+    expect(tdn1108.forming).to.be.null
   })
 })
