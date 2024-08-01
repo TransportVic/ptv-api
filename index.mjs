4221c4c610747e4d95bd3edd182c20d43438d789
Eyeball Edward <edwardyeung39@gmail.com>
2024-08-01 10:51:06 +1000

    add index.mjs for exports

---
+++ index.mjs
@@ -0,0 +1,11 @@
+import { PTVAPIInterface } from './lib/ptv-api-interface.mjs'
+import PTVAPI from './lib/ptv-api.mjs'
+import { TramTrackerAPIInterface } from './lib/tramtracker-api-interface.mjs'
+import { VLineAPIInterface } from './lib/vline-api-interface.mjs'
+
+export {
+  PTVAPIInterface,
+  PTVAPI,
+  TramTrackerAPIInterface,
+  VLineAPIInterface
+}
