6e16ec3c109e8f5e9d7ed99dc3850629716dab27
Eyeball Edward <edwardyeung39@gmail.com>
2025-03-30 19:27:09 +1100

    export MetroSiteAPIInterface as well

--- index.mjs
+++ index.mjs
@@ -3,11 +3,13 @@ import PTVAPI from './lib/ptv-api.mjs'
 import { TramTrackerAPIInterface } from './lib/tramtracker-api-interface.mjs'
 import { VLineAPIInterface } from './lib/vline-api-interface.mjs'
 import { StubAPI } from './stub-api.mjs'
+import { MetroSiteAPIInterface } from './lib/metro-site-api-interface.mjs'
 
 export {
   PTVAPIInterface,
   PTVAPI,
   TramTrackerAPIInterface,
   VLineAPIInterface,
+  MetroSiteAPIInterface,
   StubAPI
 }
