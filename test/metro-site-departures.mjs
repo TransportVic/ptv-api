45cb3319a27535a1537830a68e9c211158e6d546
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-09 21:29:23 +1000

    implemented metro site departures

--- test/metro-site-departures.mjs
+++ test/metro-site-departures.mjs
@@ -14,9 +14,9 @@ describe('The MetroSiteDeparture class', () => {
       let ptvAPI = new PTVAPI(stubAPI)
       ptvAPI.addMetroSite(stubAPI)
 
-      let willamstown = await ptvAPI.metroSite.getDepartures()
+      let departures = await ptvAPI.metroSite.getDepartures()
 
-      let td1054 = willamstown.find(trip => trip.tdn === '1054')
+      let td1054 = departures.find(trip => trip.tdn === '1054')
       expect(td1054).to.exist
       expect(dateLikeToISO(td1054.operationalDateMoment)).to.equal('2025-04-19T14:00:00.000Z')
       expect(td1054.operationalDate).to.equal('20250420')
@@ -28,17 +28,17 @@ describe('The MetroSiteDeparture class', () => {
       expect(dateLikeToISO(td1054Stops[0].estimatedArrival)).to.equal('2025-04-20T04:54:00.000Z')
       expect(dateLikeToISO(td1054Stops[0].estimatedDeparture)).to.equal('2025-04-20T04:54:00.000Z')
 
-      let td4406 = willamstown.find(trip => trip.tdn === '4406')
+      let td4406 = departures.find(trip => trip.tdn === '4406')
       expect(td4406).to.exist
       expect(dateLikeToISO(td4406.operationalDateMoment)).to.equal('2025-04-19T14:00:00.000Z')
       expect(td4406.operationalDate).to.equal('20250420')
 
       let td4406Stops = td4406.stops
-      expect(td4406Stops[5].stationName).to.equal('Bonbeach')
-      expect(td4406Stops[5].platform).to.equal('1')
-      expect(dateLikeToISO(td4406Stops[0].scheduledDeparture)).to.equal('2025-04-20T05:49:00.000Z')
-      expect(dateLikeToISO(td4406Stops[0].estimatedArrival)).to.equal('2025-04-20T05:50:00.000Z')
-      expect(dateLikeToISO(td4406Stops[0].estimatedDeparture)).to.equal('2025-04-20T05:51:00.000Z')
+      expect(td4406Stops[4].stationName).to.equal('Bonbeach')
+      expect(td4406Stops[4].platform).to.equal('1')
+      expect(dateLikeToISO(td4406Stops[4].scheduledDeparture)).to.equal('2025-04-20T05:49:00.000Z')
+      expect(dateLikeToISO(td4406Stops[4].estimatedArrival)).to.equal('2025-04-20T05:50:00.000Z')
+      expect(dateLikeToISO(td4406Stops[4].estimatedDeparture)).to.equal('2025-04-20T05:51:00.000Z')
     })
   })
 })
