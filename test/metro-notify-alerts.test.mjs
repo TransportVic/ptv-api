98a9ccc43a89841127b6cb63c36eb0cdc053a2b9
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-11 09:03:45 +1000

    added test for multiple lines alert

--- test/metro-notify-alerts.test.mjs
+++ test/metro-notify-alerts.test.mjs
@@ -18,7 +18,7 @@ describe('The MetroNotifyAlerts class', () => {
       let alerts = await ptvAPI.metroSite.getNotifyData()
 
       let flemington = alerts.find(alert => alert.rawID === '629190')
-      expect(flemington.lineName).to.equal('Flemington Racecourse')
+      expect(flemington.lineNames).to.equal(['Flemington Racecourse'])
       expect(flemington.startTime.toISOString()).to.equal('2025-02-14T09:57:00.000Z')
       expect(flemington.endTime.toISOString()).to.equal('2025-02-14T11:40:00.000Z')
       expect(flemington.modifiedTime.toISOString()).to.equal('2025-02-14T09:57:31.000Z')
@@ -26,6 +26,27 @@ describe('The MetroNotifyAlerts class', () => {
       expect(flemington.rawHTML).to.equal('<p>Delays up to 25 minutes due to police attending to trespassers in the Racecourse area.<\/p>\n<ul>\n<li>Trains may be held at available platforms.<\/li>\n<li>Trains may terminate\/originate at intermediate stations.<\/li>\n<\/ul>\n<p>Check information displays and listen for announcements as services may be altered at short notice.<\/p>')
       expect(flemington.html).to.equal('<p>Delays up to 25 minutes due to police attending to trespassers in the Racecourse area.<\/p>\n<ul>\n<li>Trains may be held at available platforms.<\/li>\n<li>Trains may terminate\/originate at intermediate stations.<\/li>\n<\/ul>\n<p>Check information displays and listen for announcements as services may be altered at short notice.<\/p>')
       expect(flemington.text).to.equal('Delays up to 25 minutes due to police attending to trespassers in the Racecourse area.\nTrains may be held at available platforms.\nTrains may terminate\/originate at intermediate stations.\nCheck information displays and listen for announcements as services may be altered at short notice.')
+    })
+
+    it('Should combine alerts for multiple lines into one', async () => {
+      let stubAPI = new StubAPI()
+      stubAPI.setResponses([ stubNotifyData ])
+      stubAPI.skipErrors()
+
+      let ptvAPI = new PTVAPI(stubAPI)
+      ptvAPI.addMetroSite(stubAPI)
+
+      let alerts = await ptvAPI.metroSite.getNotifyData()
+
+      let hoppers = alerts.find(alert => alert.rawID === '629186')
+      expect(hoppers.lineNames).to.have.members(['Werribee', 'Williamstown'])
+      expect(hoppers.startTime.toISOString()).to.equal('2025-02-14T09:42:00.000Z')
+      expect(hoppers.endTime.toISOString()).to.equal('2025-02-14T11:50:00.000Z')
+      expect(hoppers.modifiedTime.toISOString()).to.equal('2025-02-14T09:49:34.000Z')
+      expect(hoppers.type).to.equal('major')
+      expect(hoppers.rawHTML).to.equal('<p>Delays up to 40 minutes after police attended to a trespasser in the Hoppers Crossing area.<\/p>\n<ul>\n<li>Trains may terminate\/originate at intermediate stations while we recover the timetable.<\/li>\n<\/ul>\n<p>Check information displays and listen for announcements as services may be altered at short notice.<\/p>')
+      expect(hoppers.html).to.equal('<p>Delays up to 40 minutes after police attended to a trespasser in the Hoppers Crossing area.<\/p>\n<ul>\n<li>Trains may terminate\/originate at intermediate stations while we recover the timetable.<\/li>\n<\/ul>\n<p>Check information displays and listen for announcements as services may be altered at short notice.<\/p>')
+      expect(hoppers.text).to.equal('Delays up to 40 minutes after police attended to a trespasser in the Hoppers Crossing area.\nTrains may terminate\/originate at intermediate stations while we recover the timetable.\nCheck information displays and listen for announcements as services may be altered at short notice.')
     })
 
     it('Should return a different ID if the alert text is changed', async () => {
