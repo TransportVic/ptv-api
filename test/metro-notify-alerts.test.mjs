41b3e90e5ce969c75b58fee7c615342c41cb6599
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-11 08:37:57 +1000

    implemented basic alert data

--- test/metro-notify-alerts.test.mjs
+++ test/metro-notify-alerts.test.mjs
@@ -19,7 +19,7 @@ describe('The MetroNotifyAlerts class', () => {
       let flemington = alerts.find(alert => alert.rawID === '629190')
       expect(flemington.startTime.toISOString()).to.equal('2025-02-14T09:57:00.000Z')
       expect(flemington.endTime.toISOString()).to.equal('2025-02-14T11:40:00.000Z')
-      expect(flemington.modifiedTime.toISOString()).to.equal('2025-02-14T11:40:00.000Z')
+      expect(flemington.modifiedTime.toISOString()).to.equal('2025-02-14T09:57:31.000Z')
       expect(flemington.type).to.equal('major')
       expect(flemington.rawHTML).to.equal('<p>Delays up to 25 minutes due to police attending to trespassers in the Racecourse area.<\/p>\n<ul>\n<li>Trains may be held at available platforms.<\/li>\n<li>Trains may terminate\/originate at intermediate stations.<\/li>\n<\/ul>\n<p>Check information displays and listen for announcements as services may be altered at short notice.<\/p>\n')
       expect(flemington.html).to.equal('<p>Delays up to 25 minutes due to police attending to trespassers in the Racecourse area.<\/p>')
