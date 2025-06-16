eba102b96a46d0cd5046b7de86f23e206def2fe5
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-16 16:41:12 +1000

    test to ensure it actually works

---
+++ sample/get-vline-suspension-details.mjs
@@ -0,0 +1,18 @@
+import { PTVAPIInterface } from '../lib/ptv-api-interface.mjs'
+import PTVAPI from '../lib/ptv-api.mjs'
+import { VLineAPIInterface } from '../lib/vline-api-interface.mjs'
+import config from './config.json' with { type: 'json' }
+import inspect from './inspect.mjs'
+
+let ptvAPI = new PTVAPI(new PTVAPIInterface(config.devID, config.key))
+let vlineAPIInterface = new VLineAPIInterface(
+  config.vlineCallerID,
+  config.vlineSignature
+)
+ptvAPI.addVLine(vlineAPIInterface)
+
+async function main() {
+  console.log((await ptvAPI.vline.getLiveDisruptionsDetails()).getAlteredServices())
+}
+
+main()
