c50942c08a29cfd581f852b4e10f8dc23925efb3
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-17 00:24:16 +1000

    update sample to use current PT day

--- sample/get-metro-pattern.mjs
+++ sample/get-metro-pattern.mjs
@@ -1,3 +1,4 @@
+import { dateLikeToISO, parseISOTime } from '../lib/date-utils.mjs'
 import { PTVAPIInterface } from '../lib/ptv-api-interface.mjs'
 import PTVAPI from '../lib/ptv-api.mjs'
 import config from './config.json' with { type: 'json' }
@@ -9,9 +10,12 @@ async function main() {
   let tdn = process.argv[2]
   if (!tdn) return console.error('Please provide a TDN!')
 
+  let timeNow = parseISOTime(new Date().toISOString())
+  if (timeNow.get('hour') < 3) timeNow = timeNow.plus({ day: -1 })
+
   let trip = await ptvAPI.metro.getStoppingPatternFromTDN(tdn, {
     includeForming: true,
-    date: new Date(),
+    date: new Date(dateLikeToISO(timeNow)),
     expand: ['VehicleDescriptor', 'VehiclePosition']
   })
 
