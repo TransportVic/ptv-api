137331a295acacd81de4513b2668746df3dfcd80
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-18 11:48:09 +1000

    expose vline api classes

--- index.mjs
+++ index.mjs
@@ -2,8 +2,11 @@ import { PTVAPIInterface } from './lib/ptv-api-interface.mjs'
 import PTVAPI from './lib/ptv-api.mjs'
 import { TramTrackerAPIInterface } from './lib/tramtracker-api-interface.mjs'
 import { VLineAPIInterface } from './lib/vline-api-interface.mjs'
-import { StubAPI } from './stub-api.mjs'
+import { StubAPI, StubVLineAPI } from './stub-api.mjs'
 import { MetroSiteAPIInterface } from './lib/metro-site-api-interface.mjs'
+import { GetPlatformArrivalsAPI, GetPlatformDeparturesAPI, GetPlatformServicesAPI } from './lib/vline/get-platform-services.mjs'
+import { GetJourneyStopsAPI } from './lib/vline/get-journey-stops.mjs'
+import { GetJourneysAPI } from './lib/vline/get-journeys.mjs'
 
 export {
   PTVAPIInterface,
@@ -11,5 +14,7 @@ export {
   TramTrackerAPIInterface,
   VLineAPIInterface,
   MetroSiteAPIInterface,
-  StubAPI
+  StubAPI, StubVLineAPI,
+  GetPlatformServicesAPI, GetPlatformArrivalsAPI, GetPlatformDeparturesAPI,
+  GetJourneyStopsAPI, GetJourneysAPI
 }
